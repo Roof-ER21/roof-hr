2cd346e38a6a8a8b86df4f7ad63b80aa5d314306
Roof-ER21 <marketing@theroofdocs.com>
2026-01-08 00:11:23 -0500

    Load env in contract send script

--- server/scripts/send-contracts-for-signing.ts
+++ server/scripts/send-contracts-for-signing.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { storage } from '../storage';
 import { contractPdfService } from '../services/contractPdfService';
 import { notifyRecipientOfNewContract } from '../services/contract-notification';
